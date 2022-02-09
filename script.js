a9a1212cb0a0639e34d21e34b9922eede928246a
Xytabich <xytabich@gmail.com>
2022-02-09 11:29:52 +0300

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", function() {
 	var links = document.body.querySelectorAll("a[src]");
 	for(var i = 0; i < links.length; i++)
 	{
-		links[i].href = "?p=" + encodeURIComponent(links[i].getAttribute("src"));
+		links[i].href = "/index.html?p=" + encodeURIComponent(links[i].getAttribute("src"));
 		links[i].removeAttribute("src");
 	}
 });
