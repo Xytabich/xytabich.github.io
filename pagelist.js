d29ee20195117930ab99bf8acbf133d4256d5784
Xytabich <xytabich@gmail.com>
2022-02-09 08:49:36 +0300

    index & glassmaking

---
+++ pagelist.js
@@ -0,0 +1,14 @@
+onPagelistLoaded({
+	"main.html": {
+		"name": "Main"
+	},
+	"vintagestory": {
+		"name": "Vintage Story Mods",
+		"noLink": true,
+		"pages": {
+			"vintagestory/glassmaking/index.html": {
+				"name": "Glassmaking"
+			}
+		}
+	}
+});
