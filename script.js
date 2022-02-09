320a16a6f4c98e1101eb2f5ace4ab6b71e81c9e8
Xytabich <xytabich@gmail.com>
2022-02-09 11:14:37 +0300

    glasstype

---
+++ script.js
@@ -0,0 +1,8 @@
+document.body.addEventListener("DOMContentLoaded", function() {
+	var links = document.body.querySelectorAll("a[src]");
+	for(var i = 0; i < links.length; i++)
+	{
+		links[i].href = "?p=" + encodeURIComponent(links[i].getAttribute("src"));
+		links[i].removeAttribute("src");
+	}
+});
